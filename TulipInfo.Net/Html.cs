13f8aecc49ddd08e9159dc0dd41f87a56bbc39b2
liuwu <liuwu@outlook.com>
2022-07-20 15:00:39 +0800

    TulipInfo.Net

--- TulipInfo.Net/Html.cs
+++ TulipInfo.Net/Html.cs
@@ -13,5 +13,14 @@ namespace TulipInfo.Net
             str = rx.Replace(str, "");
             return str;
         }
+
+        public static string CleanHtml(string input)
+        {
+            string str = input;
+            System.Text.RegularExpressions.Regex rx = new System.Text.RegularExpressions.Regex("<[^>]*>|&[a-z]{2,4};");
+            str = rx.Replace(str, "");
+
+            return str;
+        }
     }
 }
